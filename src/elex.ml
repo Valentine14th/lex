34a1216ef5bfd7863016b869e324ff2bd66a9708
Jonas Degelo <degeloj@ethz.ch>
2024-09-01 21:02:06 +0200

    fix bug where renamed parameters constitutive rules propageted into html documentation

--- src/elex.ml
+++ src/elex.ml
@@ -203,7 +203,12 @@ let get_constitutive_params ecrules = function
       | _ -> assert false
     in
     let disjuncts = List.zip_exn c_rules d_indices |> List.map ~f:aux in
-    let pf = List.map disjuncts ~f:(fun d -> d.pf) |> List.hd_exn in (* TODO (potentially) check that all compilation rule have the same disjunct *)
+    let g = List.map2_exn disjuncts g ~f:(fun d g -> match g.f with
+      | EPredicate (e, _, ty) -> { g with f = EPredicate (e, d.params_original, ty) }
+      | _ -> assert false)
+    in
+    let d = List.hd_exn disjuncts in
+    let pf = d.pf in
     (pf, g)
   | _ -> assert false
 
