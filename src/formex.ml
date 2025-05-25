da38c36d87668d77ecf2a64794bbf4aac044873b
Jonas Degelo <degeloj@ethz.ch>
2025-05-25 19:21:40 +0200

    some xml paresing error reporting (only with debug=true fro now)

--- src/formex.ml
+++ src/formex.ml
@@ -2,14 +2,19 @@ open Core
 
 open LegalXml
 
-let debug_formex = ref false
+let debug_formex = ref true
 let debug msg = if !debug_formex then Errors.debug_print ~f_name:(Some "formex.ml") msg
 
 let format_ident =
   Re.replace (Re.compile (Re.(alt [char '('; char ')'; char '.']))) ~f:(fun _ -> "")
 
 let get_law_title xml =
-  let title_xml = XML.get_child_by_tag_name "TITLE" xml in
+  let title_xml =
+    try XML.get_child_by_tag_name "TITLE" xml
+    with Not_found_s msg as e ->
+      debug ("XML file does not contain a TITLE tag: " ^ Sexp.to_string_hum msg);
+      raise e
+  in
   XML.text title_xml
 
 let get_enacting_terms = XML.get_child_by_tag_name "ENACTINGTERMS"
