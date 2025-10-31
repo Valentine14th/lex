19d5375da0bf16613d987eb70205fc6589843009
francois-hublet <francois.hublet@inf.ethz.ch>
2025-10-31 17:49:53 +0100

    Minor fixes

---
+++ example/GDPR/gdpr.py
@@ -0,0 +1,23 @@
+def add_time_span(t: int, s: int) -> int:
+    return t + s
+
+def string_of_category(cat: str) -> str:
+    return cat
+
+def string_of_country_io(co: int) -> str:
+    return str(co)
+
+def string_of_declaration(de: int) -> str:
+    return str(de)
+
+def string_of_entity(c: int) -> str:
+    return str(c)
+
+def string_of_purpose(p: str) -> str:
+    return p
+
+def string_of_safeguards(sg: int) -> str:
+    return str(sg)
+
+def string_of_span(t: int) -> str:
+    return str(t)
